fb81745a6337b829ff26b3d28fc196621c7bf536
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-10-18 14:27:51 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,9 +1,5 @@
 from collections import Counter
 def solution(citations):
-    paper_cnt = Counter(citations)
-    ref = 0
-    
-    for i in range(max(paper_cnt.keys()), -1, -1):
-        ref += paper_cnt[i]
-        if ref >= i:
-            return i
+    citations.sort(reverse=True)
+    answer = max(map(min, enumerate(citations, start=1)))
+    return answer
