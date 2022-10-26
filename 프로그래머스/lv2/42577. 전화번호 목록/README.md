e63bf3e8dcb29c229831a7c0754956fca92e213d
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-10-26 18:43:40 +0900

    [level 2] Title: 전화번호 목록, Time: 91.15 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/README.md
+++ 프로그래머스/lv2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 34.6 MB, 시간: 187.60 ms
+메모리: 28 MB, 시간: 91.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,7 @@
 def solution(phone_book):
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
-    for phone_number in phone_book:
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                return False
+    phone_book = sorted(phone_book)
+    for i, v in enumerate(phone_book[1:]):
+        if v.startswith(phone_book[i]):
+            return False
+
     return True
