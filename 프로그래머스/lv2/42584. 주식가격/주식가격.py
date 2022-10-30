d4d67f49c334356cf9e71d88147e71bfff443262
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-10-30 12:58:58 +0900

    [level 2] Title: 주식가격, Time: 22.08 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,10 +1,12 @@
 def solution(prices):
+    stack = []
     answer = [0] * len(prices)
     for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                answer[i] += 1
-            else:
-                answer[i] += 1
-                break
+        if stack != []:
+            while stack != [] and stack[-1][1] > prices[i]:
+                past, _ = stack.pop()
+                answer[past] = i - past
+        stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
     return answer
