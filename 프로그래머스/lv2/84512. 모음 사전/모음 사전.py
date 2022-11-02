3906d3f0ad797e17d6f73797d2cf6f94743d3a0f
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-11-02 19:41:18 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -2,9 +2,9 @@ import itertools as it
 def solution(word):
     answer = 0
     dict_string = "AEIOU"
-    offset = [5 ** i for i in range(len(dict_string), 0, -1)]
+    offset = [len(dict_string) ** i for i in range(len(dict_string), 0, -1)]
     for i, v in enumerate(word):
-        o = sum(offset[i:]) // 5
+        o = sum(offset[i:]) // len(dict_string)
         answer += dict_string.index(v) * o + 1
     return answer
             
