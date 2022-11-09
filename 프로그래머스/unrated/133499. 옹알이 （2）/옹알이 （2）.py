ee01208036d3ea07b80df0ef3f3e6d182607f540
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-11-10 00:23:51 +0900

    [unrated] Title: 옹알이 (2), Time: 0.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -9,16 +9,16 @@ def solution(babbling):
             match = re.finditer(j, i)
             for k in match:
                 match_list.append([k.span(), j])
-                print(i, j, k)
         match_list.sort()
-        print(match_list)
         start, end = 0, 0
         last_word = ''
         for m in match_list:
             if end == m[0][0] and m[1] != last_word:
                 start, end = end, m[0][1]
                 last_word = m[1]
-        print(start, end, last_word)
+            else:
+                break
+                
         if end == len(i):
             answer += 1
                 
