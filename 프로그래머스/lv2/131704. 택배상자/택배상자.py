ae3c1914de144a9c25933eb6b55b7dac4ad945e0
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-12-14 13:48:47 +0900

    [level 2] Title: 택배상자, Time: 293.88 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.py
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -1,28 +1,35 @@
-from collections import deque
-def solution(order):
-    answer = len(order)
-    order = deque(order)
-    stack = deque()
-    queue = deque([i for i in range(1, len(order) + 1)])
-    
-    while queue:
-        box = queue.popleft()
-        if box == order[0]:
-            del order[0]
-        else:
-            if not stack:
-                stack.append(box)
-            else:
-                stack_box = stack.pop()
-                if stack_box == order[0]:
-                    del order[0]
-                    queue.appendleft(box)
-                else:
-                    stack.append(stack_box)
-                    stack.append(box)
-    
-    while stack and stack[-1] == order[0]:
-        del stack[-1]
-        del order[0]
 
-    return answer - len(order)
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+def solution(order):
+    answer = 0
+    stacks = []
+    N = len(order)
+    i = 1
+    idx = 0
+    while i < N+1:
+        stacks.append(i)
+        while stacks[-1] == order[idx]:
+            idx += 1
+            stacks.pop()
+            if len(stacks) == 0:
+                break
+        i += 1
+
+
+    return idx
