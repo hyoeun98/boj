98ec24f5eedcd112425ef4ad744e107a371dec19
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2022-12-19 18:09:22 +0900

    [unrated] Title: 디펜스 게임, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
@@ -3,8 +3,7 @@ def solution(n, k, enemy):
     minHeap = enemy[:k]
     heapq.heapify(minHeap)
     for i in range(k, len(enemy)):
-        heapq.heappush(minHeap, enemy[i])
-        n -= heapq.heappop(minHeap)
+        n -= heapq.heappushpop(minHeap, enemy[i])
         if n < 0:
             return i
     return len(enemy)
