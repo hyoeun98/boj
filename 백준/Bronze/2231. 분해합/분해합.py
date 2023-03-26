0f5245e89e8c6873acd707f420569726877e8d72
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-03-26 19:41:07 +0900

    [Bronze II] Title: 분해합, Time: 1208 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,15 +1,13 @@
 import sys
 
 N = int(sys.stdin.readline())
-is_find = False
+result = 0
 for i in range(1, N):
-     decom = list(str(i))
-     candidate = i + sum(map(int, decom))
-     if candidate == N:
-        is_find = True
-        print(i)
+    decom = str(i)
+    candidate = i + sum(map(int, decom))
+    if candidate == N:
+        result = i
         break
 
-if not is_find:
-    print(0)
+print(result)
      
