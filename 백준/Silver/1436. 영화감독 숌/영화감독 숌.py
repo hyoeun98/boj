74b0fd6677978e3791e4ad96beb62a7296c40bb3
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-04-12 19:41:44 +0900

    [Silver V] Title: 영화감독 숌, Time: 636 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,3 +1,8 @@
 N = int(input())
-demon_num = [str(i) for i in range(666, 10000000) if '666' in str(i)][:10000]
+demon_num = []
+for i in range(666, 10000000):
+    if "666" in (temp:= str(i)):
+        demon_num.append(temp)
+        if len(demon_num) > 10000:
+            break
 print(demon_num[N-1])
