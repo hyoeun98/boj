a3158cc5078ca71346c41a6e086b4a213811baf5
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-04-14 18:27:56 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    alpha_dict = {"zero" : "0", "one" : "1", "two" : "2", "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9"}
+    for i in alpha_dict.keys():
+        numbers = numbers.replace(i, alpha_dict[i])
+    return int(numbers)
