aae33af492dd4d1798c7787e6fb9c8e6254fcbcd
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-04-20 22:40:40 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,5 @@
+def solution(my_string, overwrite_string, s):
+    answer = my_string[:s] + overwrite_string
+    if len(answer) < len(my_string):
+        answer += my_string[len(answer):]
+    return answer
