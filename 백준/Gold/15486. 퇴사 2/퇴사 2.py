5fa9e88a7e4e580ec666c157d2e2e6bdefd85eb1
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-06-14 16:20:43 +0900

    [Gold V] Title: 퇴사 2, Time: 2224 ms, Memory: 168868 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -10,8 +10,8 @@ for _ in range(N):
 dp = [0] * (N + 1)
 for i in range(N-1, -1, -1):
     if i + T[i] > N:
-        dp[i] = dp[i + 1]
+        dp[i] = dp[i+1]
     else:
         dp[i] = max(dp[i+1], P[i] + dp[i+T[i]])
     
-print(dp[0])
+print(max(dp))
