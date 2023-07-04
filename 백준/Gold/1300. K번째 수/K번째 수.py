4bf2f20aa3bf3634cdf0ca942cfa3155c7791702
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-07-04 14:24:16 +0900

    [Gold II] Title: K번째 수, Time: 684 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,22 +1,23 @@
 import sys
+def bin_search(start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+
+        for i in range(1, N + 1):
+            cnt += min(mid // i, N)
+
+        # print("cnt", cnt)
+        if cnt >= k:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    return answer
 
 N = int(sys.stdin.readline())
 k = int(sys.stdin.readline())
 
 start, end = 1, k
 
-while start <= end:
-    mid = (start + end) // 2
-    cnt = 0
-
-    for i in range(1, N + 1):
-        cnt += min(mid // i, N)
-
-    # print("cnt", cnt)
-    if cnt >= k:
-        answer = mid
-        end = mid - 1
-    else:
-        start = mid + 1
-    
-print(answer)
+print(bin_search(start, end))
