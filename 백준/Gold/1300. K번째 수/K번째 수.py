6acb0880f3c74badd89f2fc6a7185630e0d7595e
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-07-04 14:22:56 +0900

    [Gold II] Title: K번째 수, Time: 1056 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,22 @@
+import sys
+
+N = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
+
+start, end = 1, k
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+
+    for i in range(1, N + 1):
+        cnt += min(mid // i, N)
+
+    # print("cnt", cnt)
+    if cnt >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+    
+print(answer)
