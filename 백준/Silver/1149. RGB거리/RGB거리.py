aaec92697d33f4f2f3c2c568f986ade8d4a535ce
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-09-14 15:54:08 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,20 +1,14 @@
 import sys
-from collections import deque
-from pprint import pprint
 
-def solution():
-    board = [[0, 0, 0]]
-    for _ in range(n):
-        board.append(list(map(int, sys.stdin.readline().split()))) # RGB
-    
-    dp = [[0 for _ in range(3)] for _ in range(n + 1)]
-    dp[1] = board[1]
-
-    for i in range(2, n + 1):
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + board[i][0]
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + board[i][1]
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + board[i][2]
-    
-    print(min(dp[-1]))
 n = int(sys.stdin.readline())
-solution()
+dp = [[0 for _ in range(3)] for _ in range(n + 1)]
+dp[1][0], dp[1][1], dp[1][2] = map(int, sys.stdin.readline().split())
+
+for i in range(2, n + 1):
+    r, g, b = map(int, sys.stdin.readline().split())
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + r
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + g
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + b
+    
+print(min(dp[-1]))
+
