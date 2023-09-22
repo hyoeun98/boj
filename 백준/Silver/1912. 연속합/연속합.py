8a1b634780775ba267a3a96957395e70d044edc9
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-09-22 14:03:17 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,13 @@
+import sys
+
+def solution():
+    arr = list(map(int, sys.stdin.readline().split()))
+    dp = [0 for _ in range(n)]
+    dp[0] = arr[0]
+
+    for i in range(1, n):
+        dp[i] = max(arr[i], dp[i - 1] + arr[i])
+    print(max(dp))
+
+n = int(sys.stdin.readline())
+solution()
