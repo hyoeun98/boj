f5e042de548f1a26bc35da58eb691d77812fc7a6
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2023-10-10 18:01:53 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -4,7 +4,7 @@ import itertools as it
 def solution():
     arr = it.permutations(range(1, n + 1), m)
     for i in arr:
-        print(*i)
+        print(" ".join(map(str, i)))
     
 n, m = map(int, sys.stdin.readline().split())
 solution()
