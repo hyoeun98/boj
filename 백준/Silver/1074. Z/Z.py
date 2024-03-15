eff04ad053dd90886b82408f8aff9ab0dafe28c4
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2024-03-15 15:48:35 +0900

    [Silver I] Title: Z, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.py
@@ -0,0 +1,26 @@
+import sys
+
+n, r, c = map(int, sys.stdin.readline().split())
+
+answer = 0
+
+while n != 0:
+    n -= 1
+    
+    if r < 2 ** n and c < 2 ** n:
+        answer += (2 ** n) * (2 ** n) * 0
+        
+    elif r < 2 ** n and c >= 2 ** n:
+        answer += (2 ** n) * (2 ** n) * 1
+        c -= (2 ** n)
+        
+    elif r >= 2 ** n and c < 2 ** n:
+        answer += (2 ** n) * (2 ** n) * 2
+        r -= (2 ** n)
+        
+    else:
+        answer += (2 ** n) * (2 ** n) * 3
+        r -= (2 ** n)
+        c -= (2 ** n)
+
+print(answer)
