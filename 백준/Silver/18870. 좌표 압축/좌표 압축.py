442b4a1a1e33b8589ec2e26df21edb5998da182b
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2024-03-22 17:42:22 +0900

    [Silver II] Title: 좌표 압축, Time: 1688 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,16 +1,9 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = list(zip(map(int, sys.stdin.readline().split()), range(n + 1)))
-answer = [0 for _ in range(n)]
-arr.sort()
-cnt = 0
-for i in range(1, n):
-    if arr[i][0] == arr[i - 1][0]:
-        answer[arr[i][1]] = answer[arr[i - 1][1]]
-        cnt += 1
-    else:
-        answer[arr[i][1]] = i - cnt
-        
-print(*answer)
+arr = list(map(int, sys.stdin.readline().split()))
 
+sorted_arr = sorted(set(arr))
+
+dict = {v:i for i, v in enumerate(sorted_arr)}
+print(*(map(lambda x: dict[x], arr)))
