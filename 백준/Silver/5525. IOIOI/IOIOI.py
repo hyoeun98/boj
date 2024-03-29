f9d9301018eb5ad0d22d7ab2573e059a7702b3b8
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2024-03-29 12:42:13 +0900

    [Silver I] Title: IOIOI, Time: 452 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,8 +1,26 @@
 import sys    
-import re
+
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 s = sys.stdin.readline().strip()
-ioi = "IO" * n + "I"
-find = list(re.finditer(f'(?=({ioi}))', s))
-print(len(find))
+
+answer = 0
+start, end = 0, 2
+ioi = "IOI"
+cnt = 0
+while end < m:
+    if s[start : end + 1] == ioi:
+        start += 2
+        end += 2
+        cnt += 1
+    else:
+        start += 1
+        end += 1
+        cnt = 0
+    
+    if cnt == n:
+        answer += 1
+        cnt -= 1
+
+print(answer)    
+    
