456e6c9a7c8334339a9502f7565603db43792520
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2024-03-30 14:48:26 +0900

    [Silver I] Title: 카잉 달력, Time: 1292 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,19 +1,19 @@
 import sys    
 import math
-t = int(sys.stdin.readline())
-for _ in range(t):
-    m, n, x, y = map(int, sys.stdin.readline().split())
+
+def solve():
     k = x
     lcm = math.lcm(m, n)
-    flag = True
     while k <= lcm:
         if (k - x) % m == 0 and (k - y) % n == 0:
-            print(k)
-            flag = False
-            break
+            return k
 
         else:
             k += m
 
-    if flag:
-        print(-1)
+    return -1
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    m, n, x, y = map(int, sys.stdin.readline().split())
+    print(solve())
