52682f797953b13bd977af7d5f3e28bf03ba46c5
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2024-09-24 17:07:09 +0900

    [Silver II] Title: 수열의 합, Time: 72 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1024. 수열의 합/수열의 합.py
@@ -0,0 +1,45 @@
+import sys
+
+n, l = map(int, sys.stdin.readline().split())
+flag = True
+while flag:
+    if (l * (l - 1) // 2) > n:
+        break
+    
+    i = max(0, n // l - l // 2 - 2)
+    temp = i * l + (l * (l - 1) // 2)
+    if temp == n:
+        answer = [a for a in range(i, i + l)]
+        flag = False
+        break
+    
+    i += 1
+    temp = i * l + (l * (l - 1) // 2)
+    if temp == n:
+        answer = [a for a in range(i, i + l)]
+        flag = False
+        break
+    
+    i += 1
+    temp = i * l + (l * (l - 1) // 2)
+    if temp == n:
+        answer = [a for a in range(i, i + l)]
+        flag = False
+        break
+    
+    i += 1
+    temp = i * l + (l * (l - 1) // 2)
+    if temp == n:
+        answer = [a for a in range(i, i + l)]
+        flag = False
+        break
+    
+    l += 1
+    
+if flag:
+    print(-1)
+else:
+    if len(answer) > 100:
+        print(-1)
+    else:
+        print(*answer)
