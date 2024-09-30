25d1255bf51448ac2b74ed2505ae87fbcac08ab0
Hyoeun <62679143+hyoeun98@users.noreply.github.com>
2024-09-30 16:16:51 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 6440 ms, Memory: 350236 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -9,8 +9,8 @@ visited = set()
 answer = 0
 for _ in range(m):
     a, b, c = map(int, sys.stdin.readline().split())
-    heappush(graph[a], (c, b))
-    heappush(graph[b], (c, a))
+    graph[a].append((b, c))
+    graph[b].append((a, c))
     
 queue = []
 queue.append((0, 1))
@@ -21,7 +21,7 @@ while queue:
         max_weight = max(max_weight, weight)
         visited.add(vertex)
         answer += weight
-        for next_weight, next_vertex in graph[vertex]:
+        for next_vertex, next_weight in graph[vertex]:
             if next_vertex not in visited:
                 heappush(queue, (next_weight, next_vertex))
         
